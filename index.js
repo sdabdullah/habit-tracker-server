d71b7c8f9d81ff9222571117102e15a7fcd5d0ef
sdabdullah <ab.faishal@gmail.com>
2025-11-13 09:55:06 +0600

    habit tracker server created & connected

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb+srv://habitTrackerDB:QBV24f2UE6Y1Hymr@cluster0.xligdge.mongodb.net/?appName=Cluster0";
+
+// MongoClient
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('Habit server is running')
+})
+
+
+async function run() {
+    try {
+        await client.connect();
+
+        
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+    console.log(`Habit server is running on port, ${port}`);
+
+})
+
